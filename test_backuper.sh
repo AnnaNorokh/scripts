1cbee6ed0b4f6582105bd3f0794040f23f861b2e
AnnaNorokh <annanorokh@gmail.com>
2022-06-20 12:33:14 +0300

    Fixed tests

--- test_backuper.sh
+++ test_backuper.sh
@@ -8,27 +8,27 @@ COUNT=`ls -a ~/backup | wc -l`
 ~/ramp_up/backuper.sh create
 COUNT2=`ls -a ~/backup | wc -l`
 echo -e "//////   TEST  1   //////  "
-[ "$COUNT" != "$COUNT2" ]; echo -e "${GREEN}OK${NC}" || echo -e "${RED}FAILED${NC}"
+[ "$COUNT" != "$COUNT2" ] && echo -e "${GREEN}OK${NC}" || echo -e "${RED}FAILED${NC}"
 
-~/ramp_up/backuper.sh create file 
 echo -e "//////   TEST  2   //////  " 
-[ "$?" = "9" ]; echo -e "${GREEN}OK${NC} \n" || echo -e "${RED}FAILED${NC} \n"
+~/ramp_up/backuper.sh create file 
+[ $? -eq 99 ] && echo -e "${GREEN}OK${NC} \n" || echo -e "${RED}FAILED${NC} \n"
 
-~/ramp_up/backuper.sh restore 
 echo -e "//////   TEST  3   //////  "
-[ "$?" = "9" ]; echo -e "${GREEN}OK${NC}\n" || echo -e "${RED}FAILED${NC}\n"
+~/ramp_up/backuper.sh restore 
+[ $? -eq 99 ] && echo -e "${GREEN}OK${NC}\n" || echo -e "${RED}FAILED${NC}\n"
 
 echo -e "//////   TEST  4   //////  "
 ~/ramp_up/backuper.sh restore PROJ_2022-06-17_20-43-23
-[ -d ./PROJ_2022-06-17_20-43-23/ ]; echo -e "${GREEN}OK${NC}\n" || echo -e "${RED}FAILED${NC}\n"
+[ -d ./PROJ_2022-06-17_20-43-23/ ] && echo -e "${GREEN}OK${NC}\n" || echo -e "${RED}FAILED${NC}\n"
 rm -R -f ./PROJ_2022-06-17_20-43-23/
 
 echo -e "//////   TEST 5   //////  "
 ~/ramp_up/backuper.sh restore PROJ_2022-06-17_20-43
-[ "$?" = "9" ]; echo -e "${GREEN}OK${NC}" || echo -e "${RED}FAILED${NC}"
+[ $? -eq 99 ] && echo -e "${GREEN}OK${NC}" || echo -e "${RED}FAILED${NC}"
 
 echo -e "//////   TEST 6   //////  "
 ~/ramp_up/backuper.sh restore PROJ_2022-06-17_20-43 sdfgh
-[ "$?" = "9" ]; echo -e "${GREEN}OK${NC}" || echo -e "${RED}FAILED${NC}"
+[ $? -eq 99 ] && echo -e "${GREEN}OK${NC}" || echo -e "${RED}FAILED${NC}"
 
 
--- test_installer.sh
+++ test_installer.sh
@@ -8,47 +8,50 @@ NC='\033[0m' # No Color
 cd ~/test && ~/ramp_up/installer.sh -i ~/backup
 
 echo -e "${PURPLE}INSTALATION${NC}"
-[ -e ~/.my-settings-test ]; echo -e "MY-SETTINGS CREATED    ${GREEN}OK${NC} " || echo -e "MY-SETTINGS CREATED ${RED}FAILED${NC}"
-[ -e ~/test.tar.gz ]; echo -e "TAR CREATED            ${GREEN}OK${NC} " || echo -e "TAR CREATED ${RED}FAILED${NC}"
-[ -d ~/backup/test ]; echo -e "DIR INSTALLED          ${GREEN}OK${NC} \n" || echo -e "${RED}FAILED${NC}\n"
+[ -e ~/.my-settings-test ] && echo -e "MY-SETTINGS CREATED    ${GREEN}OK${NC} " || echo -e "MY-SETTINGS CREATED ${RED}FAILED${NC}"
+[ -e ~/test.tar.gz ] && echo -e "TAR CREATED            ${GREEN}OK${NC} " || echo -e "TAR CREATED ${RED}FAILED${NC}"
+[ -d ~/backup/test ] && echo -e "DIR INSTALLED          ${GREEN}OK${NC} \n" || echo -e "${RED}FAILED${NC}\n"
 
-cd ~/backup/test && ~/ramp_up/installer.sh -r ~/backup
-[ "$#" == "1" ]; echo -e "${GREEN}OK${NC} \n" || echo -e "${RED}FAILED${NC} \n"
-cd ~/backup && ~/ramp_up/installer.sh -r ~/backup
-[ "$#" == "1" ]; echo -e "${GREEN}OK${NC} \n" || echo -e "${RED}FAILED${NC} \n"
-cd ~/backup/test && ~/ramp_up/installer.sh -r ~/backup123
-[ "$#" == "1" ]; echo -e "${GREEN}OK${NC} \n" || echo -e "${RED}FAILED${NC} \n"
+cd ~/backup/test  && ~/ramp_up/installer.sh -r ~/backup
+[ $? -eq 1 ] && echo -e "${GREEN}OK${NC} \n" || echo -e "${RED}FAILED${NC} \n"
+cd ~/backup  && ~/ramp_up/installer.sh -r ~/backup
+[ $? -eq 1 ] && echo -e "${GREEN}OK${NC} \n" || echo -e "${RED}FAILED${NC} \n"
+cd ~/backup/test  && ~/ramp_up/installer.sh -r ~/backup123
+[ $? -eq 1 ] && echo -e "${GREEN}OK${NC} \n" || echo -e "${RED}FAILED${NC} \n"
 
-cd ~/backup/test && ~/ramp_up/installer.sh -r ~/Files
+cd ~/backup/test  && ~/ramp_up/installer.sh -r ~/Files
 
 echo -e "\n${PURPLE}REINSTALATION${NC}"
-[ -e ~/.my-settings-test-old ]; echo -e "MY-SETTINGS-OLD CREATED ${GREEN}OK${NC} " || echo -e "MY-SETTINGS-OLD CREATED ${RED}FAILED${NC}"
-[ -e ~/.my-settings-test ]; echo -e "MY-SETTINGS CREATED     ${GREEN}OK${NC} " || echo -e "MY-SETTINGS CREATED     ${RED}FAILED${NC}"
-[ -d ~/Files/test ]; echo -e "DIR INSTALLED           ${GREEN}OK${NC} \n" || echo -e "DIR INSTALLED           ${RED}FAILED${NC}\n"
+[ -e ~/.my-settings-test-old ] && echo -e "MY-SETTINGS-OLD CREATED ${GREEN}OK${NC} " || echo -e "MY-SETTINGS-OLD CREATED ${RED}FAILED${NC}"
+[ -e ~/.my-settings-test ] && echo -e "MY-SETTINGS CREATED     ${GREEN}OK${NC} " || echo -e "MY-SETTINGS CREATED     ${RED}FAILED${NC}"
+[ -d ~/Files/test ] && echo -e "DIR INSTALLED           ${GREEN}OK${NC} \n" || echo -e "DIR INSTALLED           ${RED}FAILED${NC}\n"
 
 ~/ramp_up/installer.sh -u test
 
 echo -e "\n${PURPLE}UNINSTALATION${NC}"
-[ ! -e ~/.my-settings-test-old ]; echo -e "MY-SETTINGS REMOVER     ${GREEN}OK${NC} " || echo -e "MY-SETTINGS REMOVER ${RED}FAILED${NC}"
-[ ! -e ~/.my-settings-test ]; echo -e "MY-SETTINGS-OLD REMOVER ${GREEN}OK${NC} " || echo -e "MY-SETTINGS-OLD REMOVER ${RED}FAILED${NC}"
-[ ! -d ~/backup/test ]; echo -e "DIR REMOVER             ${GREEN}OK${NC} " || echo -e "DIR REMOVER             ${RED}FAILED${NC}"
-[ ! -d ~/Files/test ]; echo -e "DIR REMOVER             ${GREEN}OK${NC} \n" || echo -e "DIR REMOVER             ${RED}FAILED${NC}\n"
+[ ! -e ~/.my-settings-test-old ] && echo -e "MY-SETTINGS REMOVER     ${GREEN}OK${NC} " || echo -e "MY-SETTINGS REMOVER ${RED}FAILED${NC}"
+[ ! -e ~/.my-settings-test ] && echo -e "MY-SETTINGS-OLD REMOVER ${GREEN}OK${NC} " || echo -e "MY-SETTINGS-OLD REMOVER ${RED}FAILED${NC}"
+[ ! -d ~/backup/test ] && echo -e "OLD DIR REMOVER         ${GREEN}OK${NC} " || echo -e "DIR REMOVER             ${RED}FAILED${NC}"
+[ ! -d ~/Files/test ] && echo -e "DIR REMOVER             ${GREEN}OK${NC} \n" || echo -e "DIR REMOVER             ${RED}FAILED${NC}\n"
 
-#Return enviroment
+#Restore enviroment
 tar -xzf ~/test.tar.gz -C ~ && mv ~/home/anna/backup/test ~/test && rm -R ~/home && rm ~/test.tar.gz
 
-cd ~/test && ~/ramp_up/installer.sh -i 
+cd ~/test && ~/ramp_up/installer.sh -i
 
 echo -e "${PURPLE}INSTALATION to ~/local${NC}"
-[ -e ~/.my-settings-test ]; echo -e "MY-SETTINGS CREATED    ${GREEN}OK${NC} " || echo -e "MY-SETTINGS CREATED ${RED}FAILED${NC}"
-[ -e ~/test.tar.gz ]; echo -e "TAR CREATED            ${GREEN}OK${NC} " || echo -e "TAR CREATED ${RED}FAILED${NC}"
-[ -d ~/local/test ]; echo -e "DIR INSTALLED          ${GREEN}OK${NC} \n" || echo -e "${RED}FAILED${NC}\n"
+[ -e ~/.my-settings-test ] && echo -e "MY-SETTINGS CREATED    ${GREEN}OK${NC} " || echo -e "MY-SETTINGS CREATED ${RED}FAILED${NC}"
+[ -e ~/test.tar.gz ] && echo -e "TAR CREATED            ${GREEN}OK${NC} " || echo -e "TAR CREATED ${RED}FAILED${NC}"
+[ -d ~/local/test ] && echo -e "DIR INSTALLED          ${GREEN}OK${NC} \n" || echo -e "${RED}FAILED${NC}\n"
 
+#Restore enviroment
+mv ~/local/test ~ && rm ~/.my-settings-test
+
+cd ~/test && ~/ramp_up/installer.sh -i ~/backup
 cd ~/backup/test && ~/ramp_up/installer.sh -r
 
 echo -e "\n${PURPLE}REINSTALATION  to ~/local${NC}"
-[ -e ~/.my-settings-test-old ]; echo -e "MY-SETTINGS-OLD CREATED ${GREEN}OK${NC} " || echo -e "MY-SETTINGS-OLD CREATED ${RED}FAILED${NC}"
-[ -e ~/.my-settings-test ]; echo -e "MY-SETTINGS CREATED     ${GREEN}OK${NC} " || echo -e "MY-SETTINGS CREATED     ${RED}FAILED${NC}"
-[ -d ~/Files/test ]; echo -e "DIR INSTALLED           ${GREEN}OK${NC} \n" || echo -e "DIR INSTALLED           ${RED}FAILED${NC}\n"
+[ -e ~/.my-settings-test-old ] && echo -e "MY-SETTINGS-OLD CREATED ${GREEN}OK${NC} " || echo -e "MY-SETTINGS-OLD CREATED ${RED}FAILED${NC}"
+[ -e ~/.my-settings-test ] && echo -e "MY-SETTINGS CREATED     ${GREEN}OK${NC} " || echo -e "MY-SETTINGS CREATED     ${RED}FAILED${NC}"
+[ -d ~/local/test ] && echo -e "DIR INSTALLED           ${GREEN}OK${NC} \n" || echo -e "DIR INSTALLED           ${RED}FAILED${NC}\n"
 
-#~/ramp_up/installer.sh -u test
